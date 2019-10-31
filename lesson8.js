27c0973f11e26ca5c1e04bf46905b840cbb3bb43
loganmulvihill <mulvihill.logan@gmail.com>
2019-10-30 20:52:50 -0700

    error

--- lesson8.js
+++ lesson8.js
@@ -3,13 +3,14 @@
 var people = [
     { name: "John", age: 55 },
     { name: "Roger", age: 44 },
-    { name: "Betty", age: 37 }
+    { name: "Betty", age: 37 },
     ]
 
 var nameFilter = function() {
     for (var i = 0; i < people.length; i++) {
         let name = people[i].name
-        if (name > 50) {
+        let age = people[i].age
+        if (age < 50) {
             console.log("My Name is " + name)
         }
     }
